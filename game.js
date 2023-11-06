919c1eaf8213cfcb9e8208fe1bc0d9e987bbcec2
Dominic Cummings <dominiccummings95@gmail.com>
2023-11-06 10:29:24 -0600

    Question and choices being populated

--- game.js
+++ game.js
@@ -38,3 +38,25 @@ let questions = [
 //CONSTANTS 
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
+
+let startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  console.log(availableQuestions);
+  getNewQuestion();
+};
+
+let getNewQuestion = () => {
+  questionCounter++;
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach(choice => {
+    const number = choice.dataset['number'];
+    choice.innerText = currentQuestion['choice' + number];
+  });
+};
+
+startGame();
