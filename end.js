c92714579040e33980ecb138d64dc91ea46f904d
Dominic Cummings <dominiccummings95@gmail.com>
2023-11-06 12:14:23 -0600

    Top 5 Highscores are saving

--- end.js
+++ end.js
@@ -2,7 +2,14 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem("highscores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+//console.log(highScores);
+
 finalScore.innerText = mostRecentScore;
+
 username.addEventListener('keyup', () => {
   saveScoreBtn.disabled = !username.value;
 });
@@ -10,4 +17,17 @@ username.addEventListener('keyup', () => {
 let saveHighScore = (e) => {
   console.log('Clicked the save btn');
   e.preventDefault();
+
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value
+  };
+  //Add score, sort it, then splice to only be top 5
+  highScores.push(score);
+  highScores.sort( (a,b) => b.score - a.score);
+  highScores.splice(5);
+  //Save items into local storage
+  localStorage.setItem("highscores", JSON.stringify(highScores));
+  window.location.assign("/");
+  //console.log(highScores);
 };
